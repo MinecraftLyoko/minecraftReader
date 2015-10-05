7921d5cd4211796acfa7eb1a606543ea4094464c
Rhett Rogers <serenade.xs@icloud.com>
2015-10-05 13:48:47 -0600

    Initial Commit

---
+++ minecraftReader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  minecraftReader
+//
+//  Created by Rhett Rogers on 10/5/15.
+//  Copyright © 2015 Rhett Rogers. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
