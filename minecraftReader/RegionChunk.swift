3d7c06132e110f95f662ba0a7909f10ba9b05db8
Rhett Rogers <serenade.xs@icloud.com>
2015-10-07 10:27:20 -0600

    Added a bit of chunk parsing

--- minecraftReader/RegionChunk.swift
+++ minecraftReader/RegionChunk.swift
@@ -32,6 +32,15 @@ class RegionChunk {
     var chunkData: NSData
     var chunkNBT: NBTTag
     
+    var chunkLocation: (x: Int, z: Int)?
+    var biomes: [NSData]?
+    var lightPopulated: NSData?
+    var heightMap: [Int]?
+    var data: [NSData]?
+    var skyLight: [NSData]?
+    var blockLight: [NSData]?
+    var blocks: [NSData]?
+    
     init(data: NSData) {
 
         
@@ -48,8 +57,51 @@ class RegionChunk {
         chunkData = chunkData.zlibInflate()
         chunkNBT = NBTTag(data: chunkData)
         
+        print(chunkNBT.description)
+        processChunk()
+//        print("\(chunkLocation)")
         
-        
+    }
+    
+    private func processChunk() {
+        if let c = chunkNBT.compoundValue {
+            for x in c {
+                if let name = x.tagName where name == "Level" {
+                    if let levelCompound = x.compoundValue {
+                        for y in levelCompound {
+                            var tempPos = (x: 0, z: 0)
+                            var foundPos = false
+                            if let name = y.tagName {
+                                switch name {
+                                case "Biomes":
+                                    biomes = y.byteArrayValue
+                                case "LightPopulated":
+                                    lightPopulated = y.byteValue
+                                case "xPos":
+                                    if let i = y.intValue {
+                                        tempPos.x = i
+                                        foundPos = true
+                                    }
+                                case "zPos":
+                                    if let i = y.intValue {
+                                        tempPos.z = i
+                                        foundPos = true
+                                    }
+                                default: break
+                                }
+                                if foundPos {
+                                    chunkLocation = tempPos
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
     }
     
 }
